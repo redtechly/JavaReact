79a89b1927bf0fcd7df325cde6174044ad5e7405
fathy2028 <fathy2111262@miuegypt.edu.eg>
2024-03-13 20:10:57 +0200

    about us

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,6 +25,7 @@ import TestPage from "./pages/TestPage";
 import ProfileScreen from "./components/ProfileScreen";
 import ListUserComponent from "./components/ListUserComponent";
 import ChatPage from "./pages/ChatPage";
+import AboutUsScreen from "./pages/AboutUsScreen";
 // import Contact from "./pages/Contact";
 // import AboutUs from "./pages/AboutUs";
 export const REST_API_BASE_URL = "http://localhost:8081/api";
@@ -50,6 +51,7 @@ function App() {
             <Route path="/delete-role" element={<DeleteRole />} />
             <Route path="/Permissionpage" element={<Permissionpage />} />
             <Route path="/chatpage" element={<ChatPage />} />
+            <Route path="/about-us" element={<AboutUsScreen/>} />
             
               <Route
                 path="/list-product"
