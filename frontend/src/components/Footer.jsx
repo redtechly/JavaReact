4e4c5904f2966606ec4dd4b81b682b39c4ea8d10
hazem mohamed <hazemm7622@gmail.com>
2024-03-18 00:25:17 +0200

    Footer section

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,96 @@
 import React from "react";
+import {
+  Box,
+  FooterContainer,
+  Row,
+  Column,
+  FooterLink,
+  Heading,
+} from "./footerstyles";
 
-function Footer() {
-  return <footer>{/* Footer Content */}</footer>;
-}
-
+const Footer = () => {
+  return (
+    <Box>
+      <h1
+        style={{
+          color: "white",
+          textAlign: "center",
+          marginTop: "10px",
+        }}
+      >
+        A phamracy owned by the best doctors in town.
+      </h1>
+      <FooterContainer>
+        <Row>
+          <Column>
+            <Heading>About Us</Heading>
+            <FooterLink href="#">Aim</FooterLink>
+            <FooterLink href="#">Vision</FooterLink>
+            <FooterLink href="#">Testimonials</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Services</Heading>
+            <FooterLink href="#">medications</FooterLink>
+            <FooterLink href="#">Prescription</FooterLink>
+            <FooterLink href="#">analysis</FooterLink>
+            <FooterLink href="#">health care</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Contact Us</Heading>
+            <FooterLink href="#">01226186609 </FooterLink>
+            
+            
+          </Column>
+          <Column>
+            <Heading>Social Media</Heading>
+            <FooterLink href="#">
+              <i className="fab fa-facebook-f">
+                <span
+                  style={{
+                    marginLeft: "10px",
+                  }}
+                >
+                  Facebook
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-instagram">
+                <span
+                  style={{
+                    marginLeft: "10px",
+                  }}
+                >
+                  Instagram
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-twitter">
+                <span
+                  style={{
+                    marginLeft: "10px",
+                  }}
+                >
+                  Twitter
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-youtube">
+                <span
+                  style={{
+                    marginLeft: "10px",
+                  }}
+                >
+                  Youtube
+                </span>
+              </i>
+            </FooterLink>
+          </Column>
+        </Row>
+      </FooterContainer>
+    </Box>
+  );
+};
 export default Footer;
