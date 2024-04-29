202ec2a42635715bd79076d26e43754f34688a9b
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2024-04-29 11:02:16 +0300

    Add authentication logic to UserController and log user ID in log aspect

--- backend/src/main/java/net/javaguides/ems/aop/log.java
+++ backend/src/main/java/net/javaguides/ems/aop/log.java
@@ -1,23 +1,27 @@
 package net.javaguides.ems.aop;
 
 import org.aspectj.lang.JoinPoint;
-// import org.aspectj.lang.ProceedingJoinPoint;
-// import org.aspectj.lang.annotation.After;
-// import org.aspectj.lang.annotation.AfterReturning;
-// import org.aspectj.lang.annotation.Around;
-// import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import net.javaguides.ems.service.AuthenticationService;
 
 @Component
 @Aspect
 
 public class log {
+    @Autowired
+    private AuthenticationService authService;
 
     @Before("within(net.javaguides.ems.controller.*)")
     public void afterControllersInGeneral(JoinPoint joinPoint) {
-        System.out.println("Before controller method : " + joinPoint.getSignature());
+        HttpServletRequest request = (HttpServletRequest) joinPoint.getArgs()[0];
+        String authHeader = request.getHeader("Authorization");
+        String jwtToken = authHeader.substring(7);
+        System.out.println(authService.getUserFromToken(jwtToken).getId());
     }
 
 }
