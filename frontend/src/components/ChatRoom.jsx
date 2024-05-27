a3ab1379e48a7bc05bef89137709d856c514abb4
hazem mohamed <hazemm7622@gmail.com>
2024-05-27 02:07:46 +0200

    chat page + configure tesseract js in sending images

--- frontend/src/components/ChatRoom.jsx
+++ frontend/src/components/ChatRoom.jsx
@@ -1,114 +1,133 @@
-// App.js
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
+import io from 'socket.io-client';
+import '../../css/chat.css';
 
-import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
+const socket = io('http://localhost:5000'); 
 
-function App() {
-  const [username, setUsername] = useState('');
+const ChatApp = () => {
+  const [username, setUsername] = useState(localStorage.getItem('username') || '');
+  const [message, setMessage] = useState('');
+  const [file, setFile] = useState(null);
   const [messages, setMessages] = useState([]);
-  const [stompClient, setStompClient] = useState(null);
 
-  const colors = [
-    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
-    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
-  ];
-
-  const connect = (event) => {
-    if (username.trim()) {
-      // Connect to WebSocket server
-      const socket = new SockJS('http://localhost:8085/ws');
-      const client = Stomp.over(socket);
-      client.connect({}, onConnected, onError);
-      setStompClient(client);
+  useEffect(() => {
+    if (!username) {
+      Swal.fire({
+        title: 'Please enter your name',
+        input: 'text',
+        inputLabel: 'Username',
+        inputPlaceholder: 'Enter your name',
+        allowOutsideClick: false,
+        inputValidator: (value) => {
+          if (!value) {
+            return 'You need to enter a username';
+          }
+        },
+        confirmButtonText: 'Enter Chat',
+        showLoaderOnConfirm: true,
+        preConfirm: (usernameInput) => {
+          setUsername(usernameInput);
+          localStorage.setItem('username', usernameInput);
+          socket.emit('username', usernameInput);
+        },
+      });
+    } else {
+      socket.emit('username', username);
     }
-    event.preventDefault();
+
+    socket.on('userJoined', (username) => {
+      addMessage({ Author: username, Content: `${username} has joined the chat room`, system: true });
+    });
+
+    socket.on('user left', (username) => {
+      addMessage({ Author: username, Content: `${username} has left the chat room`, system: true });
+    });
+
+    socket.on('chat message', (msg) => {
+      addMessage(msg);
+    });
+
+    return () => {
+      socket.off('userJoined');
+      socket.off('user left');
+      socket.off('chat message');
+    };
+  }, [username]);
+
+  const addMessage = (msg) => {
+    setMessages((prevMessages) => [...prevMessages, msg]);
+    scrollToBottom();
   };
 
-  const onConnected = () => {
-    // Subscribe to the Public Topic
-    stompClient.subscribe('/topic/public', onMessageReceived);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
+    if (!file && !message) {
+      alert('Please enter a message');
+      return;
+    }
 
-    // Tell your username to the server
-    stompClient.send("/app/chat.addUser", {}, JSON.stringify({ sender: username, type: 'JOIN' }));
-  };
-
-  const onError = (error) => {
-    console.error('Could not connect to WebSocket server:', error);
-  };
-
-  const sendMessage = (event) => {
-    const messageInput = document.querySelector('#message');
-    const messageContent = messageInput.value.trim();
-    if (messageContent && stompClient) {
-      const chatMessage = {
-        sender: username,
-        content: messageInput.value,
-        type: 'CHAT'
+    if (file) {
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        socket.emit('chat message', {
+          Author: username,
+          Content: message,
+          image: reader.result,
+        });
+        setMessage('');
+        setFile(null);
       };
-      stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
-      messageInput.value = '';
+    } else {
+      socket.emit('chat message', {
+        Author: username,
+        Content: message,
+        image: null,
+      });
+      setMessage('');
     }
-    event.preventDefault();
   };
 
-  const onMessageReceived = (payload) => {
-    const message = JSON.parse(payload.body);
-    setMessages(prevMessages => [...prevMessages, message]);
-  };
-
-  const getAvatarColor = (messageSender) => {
-    let hash = 0;
-    for (let i = 0; i < messageSender.length; i++) {
-      hash = 31 * hash + messageSender.charCodeAt(i);
-    }
-    const index = Math.abs(hash % colors.length);
-    return colors[index];
+  const scrollToBottom = () => {
+    const messageList = document.getElementById('messages');
+    messageList.scrollTop = messageList.scrollHeight;
   };
 
   return (
-    <div className="App">
-      <div id="username-page">
-        <div className="username-page-container">
-          <h1 className="title">Type your username to enter the Chatroom</h1>
-          <form id="usernameForm" name="usernameForm" onSubmit={connect}>
-            <div className="form-group">
-              <input type="text" id="name" placeholder="Username" autoComplete="off" className="form-control" onChange={(e) => setUsername(e.target.value)} />
-            </div>
-            <div className="form-group">
-              <button type="submit" className="accent username-submit">Start Chatting</button>
-            </div>
-          </form>
-        </div>
-      </div>
-
-      <div id="chat-page" className="hidden">
-        <div className="chat-container">
-          <div className="chat-header">
-            <h2>Chat Room</h2>
-          </div>
-          <div className="connecting">
-            Connecting...
-          </div>
-          <ul id="messageArea">
-            {messages.map((message, index) => (
-              <li key={index}>
-                {/* Render messages here */}
-              </li>
-            ))}
-          </ul>
-          <form id="messageForm" name="messageForm" onSubmit={sendMessage}>
-            <div className="form-group">
-              <div className="input-group clearfix">
-                <input type="text" id="message" placeholder="Type a message..." autoComplete="off" className="form-control" />
-                <button type="submit" className="primary">Send</button>
-              </div>
-            </div>
-          </form>
-        </div>
+    <div>
+      <br />
+      <h1 style={{ textAlign: 'center' }}>Chat Room</h1>
+      <br />
+      <br />
+      <div id="chat">
+        <ul id="messages">
+          {messages.map((msg, index) => (
+            <li key={index} className="chat-message">
+              <span className="chat-username">{msg.Author}</span>: {msg.Content}
+              {msg.image && <img src={msg.image} alt="chat-img" className="image" />}
+            </li>
+          ))}
+        </ul>
+        <form id="chatform" onSubmit={handleSubmit}>
+          <input
+            id="message"
+            type="text"
+            placeholder="Enter your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <input
+            id="fileinput"
+            type="file"
+            accept="image/*"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </div>
   );
-}
+};
 
-export default App;
+export default ChatApp;
