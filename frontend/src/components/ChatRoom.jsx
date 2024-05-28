6351f893347881f84afe354266b0fadb57911df7
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2024-05-29 00:45:43 +0300

    chore: Update frontend ChatRoom component and dependencies

--- frontend/src/components/ChatRoom.jsx
+++ frontend/src/components/ChatRoom.jsx
@@ -1,57 +1,67 @@
-import React, { useEffect, useState } from 'react';
-import Swal from 'sweetalert2';
-import io from 'socket.io-client';
-import '../../css/chat.css';
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import io from "socket.io-client";
+import "../../css/chat.css";
 
-const socket = io('http://localhost:5000'); 
+const socket = io("http://chat:5000");
 
 const ChatApp = () => {
-  const [username, setUsername] = useState(localStorage.getItem('username') || '');
-  const [message, setMessage] = useState('');
+  const [username, setUsername] = useState(
+    localStorage.getItem("username") || ""
+  );
+  const [message, setMessage] = useState("");
   const [file, setFile] = useState(null);
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     if (!username) {
       Swal.fire({
-        title: 'Please enter your name',
-        input: 'text',
-        inputLabel: 'Username',
-        inputPlaceholder: 'Enter your name',
+        title: "Please enter your name",
+        input: "text",
+        inputLabel: "Username",
+        inputPlaceholder: "Enter your name",
         allowOutsideClick: false,
         inputValidator: (value) => {
           if (!value) {
-            return 'You need to enter a username';
+            return "You need to enter a username";
           }
         },
-        confirmButtonText: 'Enter Chat',
+        confirmButtonText: "Enter Chat",
         showLoaderOnConfirm: true,
         preConfirm: (usernameInput) => {
           setUsername(usernameInput);
-          localStorage.setItem('username', usernameInput);
-          socket.emit('username', usernameInput);
+          localStorage.setItem("username", usernameInput);
+          socket.emit("username", usernameInput);
         },
       });
     } else {
-      socket.emit('username', username);
+      socket.emit("username", username);
     }
 
-    socket.on('userJoined', (username) => {
-      addMessage({ Author: username, Content: `${username} has joined the chat room`, system: true });
+    socket.on("userJoined", (username) => {
+      addMessage({
+        Author: username,
+        Content: `${username} has joined the chat room`,
+        system: true,
+      });
     });
 
-    socket.on('user left', (username) => {
-      addMessage({ Author: username, Content: `${username} has left the chat room`, system: true });
+    socket.on("user left", (username) => {
+      addMessage({
+        Author: username,
+        Content: `${username} has left the chat room`,
+        system: true,
+      });
     });
 
-    socket.on('chat message', (msg) => {
+    socket.on("chat message", (msg) => {
       addMessage(msg);
     });
 
     return () => {
-      socket.off('userJoined');
-      socket.off('user left');
-      socket.off('chat message');
+      socket.off("userJoined");
+      socket.off("user left");
+      socket.off("chat message");
     };
   }, [username]);
 
@@ -64,40 +74,40 @@ const ChatApp = () => {
     e.preventDefault();
     const reader = new FileReader();
     if (!file && !message) {
-      alert('Please enter a message');
+      alert("Please enter a message");
       return;
     }
 
     if (file) {
       reader.readAsDataURL(file);
       reader.onload = () => {
-        socket.emit('chat message', {
+        socket.emit("chat message", {
           Author: username,
           Content: message,
           image: reader.result,
         });
-        setMessage('');
+        setMessage("");
         setFile(null);
       };
     } else {
-      socket.emit('chat message', {
+      socket.emit("chat message", {
         Author: username,
         Content: message,
         image: null,
       });
-      setMessage('');
+      setMessage("");
     }
   };
 
   const scrollToBottom = () => {
-    const messageList = document.getElementById('messages');
+    const messageList = document.getElementById("messages");
     messageList.scrollTop = messageList.scrollHeight;
   };
 
   return (
     <div>
       <br />
-      <h1 style={{ textAlign: 'center' }}>Chat Room</h1>
+      <h1 style={{ textAlign: "center" }}>Chat Room</h1>
       <br />
       <br />
       <div id="chat">
@@ -105,7 +115,9 @@ const ChatApp = () => {
           {messages.map((msg, index) => (
             <li key={index} className="chat-message">
               <span className="chat-username">{msg.Author}</span>: {msg.Content}
-              {msg.image && <img src={msg.image} alt="chat-img" className="image" />}
+              {msg.image && (
+                <img src={msg.image} alt="chat-img" className="image" />
+              )}
             </li>
           ))}
         </ul>
